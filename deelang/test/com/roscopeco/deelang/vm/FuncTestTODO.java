6ccf81076cb550ffaedb28e7600be2c1a43bc4d5
Ross Bamford <roscopeco@gmail.com>
2011-10-31 23:01:32 +0000

    Added stub tests for VM (To be implemented later)

---
+++ deelang/test/com/roscopeco/deelang/vm/FuncTestTODO.java
@@ -0,0 +1,18 @@
+package com.roscopeco.deelang.vm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/* TODO: Implement VM Tests.
+ * 
+ * Things to test:
+ * 
+ *    Code exec is as it should be.
+ *    Integration with the runtime.
+ */
+public class FuncTestTODO {
+  @Test
+  public void testTODO() {
+    fail("Not yet implemented");
+  }
+}
---
+++ deelang/test/com/roscopeco/deelang/vm/FunctionalTests.java
@@ -0,0 +1,11 @@
+package com.roscopeco.deelang.vm;
+
+import org.junit.runners.Suite;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  FuncTestTODO.class
+})
+public class FunctionalTests { }
