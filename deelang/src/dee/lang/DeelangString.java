27885ccc80ab3fc36c7fcd6059d1e020a532b5e8
Ross Bamford <roscopeco@gmail.com>
2012-10-05 05:06:11 +0000

    Remove setString

--- deelang/src/dee/lang/DeelangString.java
+++ deelang/src/dee/lang/DeelangString.java
@@ -41,10 +41,6 @@ public class DeelangString extends DeelangObject {
     return string;
   }
   
-  public void setString(String string) {
-    this.string = string;
-  }
-  
   @Override
   public String toString() {
     return string;
