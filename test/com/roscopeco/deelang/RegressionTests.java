94475a8398741b5c3fb7fe61a7772250eda3f62a
Ross Bamford <roscopeco@gmail.com>
2012-12-06 12:25:32 +0000

    Implement regression test (bug #1)

--- test/com/roscopeco/deelang/RegressionTests.java
+++ test/com/roscopeco/deelang/RegressionTests.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-  com.roscopeco.deelang.runtime.deevm.RegressionTests.class
+  com.roscopeco.deelang.runtime.deevm.RegressionTests.class,
+  com.roscopeco.deelang.compiler.dex.RegressionTests.class
 })
 public class RegressionTests { }
